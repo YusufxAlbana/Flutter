2195ace19618ad7eb76ecd864043c02343e3b53a
Yusufelgebrak <yusufnawafalbana2009@gmail.com>
2025-08-07 09:07:42 +0700

    test update

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 // Widget utama aplikasi
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World App',
+      title: 'Hello World with a Twist',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Flutter!'),
+          title: const Text('Hello Flutter Indonesia!'),
         ),
-        body: Center(
-          child: Text(
-            'Hello, World!',
-            style: TextStyle(fontSize: 24),
+        body: const Center(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Kenapa programmer benci alam? ',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Karena di alam nggak ada Dark Mode.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black54,
+                  ),
+                ),
+                SizedBox(height: 40),
+                Text(
+                  'Tepuk tangan untuk leluconnya, ya.',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
