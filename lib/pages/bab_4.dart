bf9e0fc6cac82c39ab8bd28f29e10e0c876d7bfe
Rifai-Hub <rifaigusnian1401@gmail.com>
2025-08-12 08:11:44 +0700

    Nyoba dulu

---
+++ lib/pages/bab_4.dart
@@ -0,0 +1,15 @@
+// Latihan Mandiri
+// 1. Buatlah sebuah proyek Flutter baru bernama simple_calculator_app. 
+// 2. Desain UI kalkulator sederhana yang memiliki: 
+// a. Dua TextField untuk memasukkan dua angka. 
+// b. Empat tombol untuk operasi: Tambah (+), Kurang (-), Kali (x), Bagi (/). 
+// c. Sebuah Text widget untuk menampilkan hasil perhitungan. 
+// 3. Gunakan TextEditingController untuk mengambil nilai dari kedua 
+// TextField. 
+// 4. Implementasikan logika perhitungan untuk setiap tombol. Ketika tombol 
+// ditekan, ambil nilai dari TextField, lakukan perhitungan, dan perbarui Text 
+// widget hasil menggunakan setState(). 
+// 5. Pastikan Anda menangani kasus di mana input bukan angka atau pembagian 
+// dengan nol (Anda bisa menampilkan pesan kesalahan di Text widget hasil). 
+// 6. (Opsional, Tingkat Lanjut) Coba implementasikan kalkulator ini menggunakan 
+// Provider untuk mengelola state dari dua angka input dan hasil perhitungan. 
