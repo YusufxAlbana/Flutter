3db6957cda32820165d6a7b203c4a99afdf245fb
Yusufelgebrak <yusufnawafalbana2009@gmail.com>
2025-08-13 08:16:28 +0700

    perubahan bab 3

--- lib/pages/bab_3.dart
+++ lib/pages/bab_3.dart
@@ -11,41 +11,53 @@ class Bab3 extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          padding: const EdgeInsets.all(20), // Padding di semua sisi
-          margin: const EdgeInsets.all(16), // tambahin margin biar nggak nempel tepi layar
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.all(16),
+          width: 350,
+          height: 400,
           decoration: BoxDecoration(
             color: Colors.lightBlueAccent,
-            borderRadius: BorderRadius.circular(50), // Sudut membulat
+            borderRadius: BorderRadius.circular(50),
           ),
           child: Column(
-            mainAxisSize: MainAxisSize.min, // Supaya container ikut ukuran isi
+            mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset(
-                'assets/images/hai.jpg',
-                width: 150,
-                height: 150,
-
+              ClipRRect(
+                borderRadius: BorderRadius.circular(75),
+                child: Image.asset(
+                  'assets/images/hai.jpg',
+                  width: 150,
+                  height: 150,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(height: 16),
               const Text(
                 'Yusuf Nawaf Albana',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
               const Text(
                 'Pekerjaan : Siswa IDN',
-                style: TextStyle(fontSize: 16),
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
               ),
               const SizedBox(height: 16),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
-                  Icon(Icons.home),
-                  SizedBox(width: 16),
-                  Icon(Icons.email),
-                  SizedBox(width: 16),
-                  Icon(Icons.chat_bubble),
+                  Icon(Icons.home, color: Colors.white, size: 32),
+                  SizedBox(width: 8),
+                  Icon(Icons.email, color: Colors.white, size: 32),
+                  SizedBox(width: 8),
+                  Icon(Icons.chat_bubble, color: Colors.white, size: 32),
                 ],
               ),
             ],
